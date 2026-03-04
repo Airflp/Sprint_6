531ae62fa9dc496f05a7cce8e4f5312f1c71b7d5
Artur Kozlov <work.tur@yandex.ru>
2026-03-04 22:21:50 +0300

    remove all files

--- locators/locators_main_page_locators.py
+++
@@ -1,9 +0,0 @@
-from locators.main_page_locators import MainPageLocators
-
-class MainPageLocators:
-    ORDER_BUTTON_TOP = (By.XPATH, "//button[contains(text(), 'Заказать')]")
-    ORDER_BUTTON_BOTTOM = (By.XPATH, "//button[contains(text(), 'Заказать')]")
-    DROPDOWN_QUESTION_1 = (By.XPATH, "//div[text()='Question 1']")
-    DROPDOWN_QUESTION_2 = (By.XPATH, "//div[text()='Question 2']")
-    LOGO_SAMOKAT = (By.XPATH, "//img[@alt='Самокат']")
-    LOGO_YANDEX = (By.XPATH, "//img[@alt='Яндекс']")
